71bd8f1686d5c8244b4fd544b3849653ab38bee5
Daria <dariamesesan@yahoo.com>
2023-10-22 23:28:20 +0300

    Add project files.

---
+++ Data/MediiDeProgramare_MesesanDaria_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MediiDeProgramare_MesesanDaria_Lab2.Models;
+
+namespace MediiDeProgramare_MesesanDaria_Lab2.Data
+{
+    public class MediiDeProgramare_MesesanDaria_Lab2Context : DbContext
+    {
+        public MediiDeProgramare_MesesanDaria_Lab2Context (DbContextOptions<MediiDeProgramare_MesesanDaria_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MediiDeProgramare_MesesanDaria_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<MediiDeProgramare_MesesanDaria_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
