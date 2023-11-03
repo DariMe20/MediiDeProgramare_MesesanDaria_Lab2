36eb19cf8ec338bc2bf1fd2ff1bc109d38b17e5c
Daria <dariamesesan@yahoo.com>
2023-11-03 10:39:47 +0200

    Added sorting options for Books and Authors in Index page of Books

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -24,10 +24,16 @@ namespace MediiDeProgramare_MesesanDaria_Lab2.Pages.Books
         public int BookID { get; set; }
         public int CategoryID { get; set; }
 
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
 
-        public async Task OnGetAsync(int? id, int? categoryID)
+
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder)
         {
             BookD = new BookData();
+
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = sortOrder == "author" ? "author_desc" : "author";
 
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
@@ -37,6 +43,7 @@ namespace MediiDeProgramare_MesesanDaria_Lab2.Pages.Books
             .AsNoTracking()
             .OrderBy(b => b.Title)
             .ToListAsync();
+
             if (id != null)
             {
                 BookID = id.Value;
@@ -44,6 +51,26 @@ namespace MediiDeProgramare_MesesanDaria_Lab2.Pages.Books
                 .Where(i => i.ID == id.Value).Single();
                 BookD.Categories = book.BookCategories.Select(s => s.Category);
             }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                   s.Title);
+                    break;
+                case "author_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                   s.Author.FullName);
+                    break;
+                case "author":
+                    BookD.Books = BookD.Books.OrderBy(s =>
+                   s.Author.FullName);
+                    break;
+                default:
+                    BookD.Books = BookD.Books.OrderBy(s => s.Title);
+                    break;
+            }
+
         }
     }
 }
