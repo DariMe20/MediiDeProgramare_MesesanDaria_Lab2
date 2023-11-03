341b012532b1bb85af9733776a1e3686fac1e595
Daria <dariamesesan@yahoo.com>
2023-11-03 10:58:40 +0200

    Created View Model class for CategoryIndexData to show all books in a certain category from Category/Index page

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace MediiDeProgramare_MesesanDaria_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
