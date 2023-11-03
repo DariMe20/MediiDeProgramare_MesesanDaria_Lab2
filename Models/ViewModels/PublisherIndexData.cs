b47acb222d81562c835602b9309ea6db11ed3476
Daria <dariamesesan@yahoo.com>
2023-11-03 10:11:29 +0200

    Created PublisherIndexData class and added Publisher and Book tables as properties

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using MediiDeProgramare_MesesanDaria_Lab2.Models;
+namespace MediiDeProgramare_MesesanDaria_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
