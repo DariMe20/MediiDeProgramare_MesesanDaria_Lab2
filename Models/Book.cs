0d0955e3bb4e4f04fd717d2f558e35c9dc0d595d
Daria <dariamesesan@yahoo.com>
2023-11-15 13:27:59 +0200

    Adaugare reguli validare pentru titlurile cartilor in clasa Book - required, si limite de lungime pentru titlu

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,22 +7,27 @@ namespace MediiDeProgramare_MesesanDaria_Lab2.Models
     public class Book
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Titlul cărții este obligatoriu.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "Lungimea titlului trebuie să fie între 3 și 150 de caractere.")]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        public Author? Author { get; set; }
 
+        public Author? Author { get; set; }
         public int AuthorID { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
         [Range(0.01, 500)]
-
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Publishing Date")]
         public DateTime PublishingDate { get; set; }
+
         public int PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
+
+
         [Display(Name = "Book Category")]
         public ICollection<BookCategory>? BookCategories { get; set; }
 
--- Models/Member.cs
+++ Models/Member.cs
@@ -8,7 +8,7 @@ namespace MediiDeProgramare_MesesanDaria_Lab2.Models
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$", ErrorMessage ="Prenumele trebuie sa inceapa cu majuscula (ex. Ana sau Ana Maria sau AnaMaria")]
         [StringLength(30, MinimumLength = 3)]
         public string? FirstName { get; set; }
-        [RegularExpression(@"^[A-Z]+[a-z\s]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$", ErrorMessage = "Numele trebuie sa inceapa cu majuscula")]
         [StringLength(30, MinimumLength = 3)]
         public string? LastName { get; set; }
         [StringLength(70)]
